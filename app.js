3a7802251d12e900fb3fd13ff5ccfc696f424e06
jm kim <rookie2000g@gmail.com>
2022-05-02 16:32:45 +0900

    Feat(app): use morgan

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const morgan = require("morgan");
 
 const { sequelize } = require("./models");
 
@@ -8,6 +9,8 @@ app.set("port", process.env.PORT || 8080);
 sequelize.sync({ force: false })
   .then(() => console.log("database is connected..."))
   .catch((error) => console.error(error));
+
+app.use(morgan("dev"));
 
 app.get("/", (req, res) => {
   res.send("express");
