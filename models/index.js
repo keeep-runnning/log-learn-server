3862097c1d3245fd990fd5e6078ba66c14697ac1
jm kim <rookie2000g@gmail.com>
2022-05-02 16:55:03 +0900

    Feat(models): define User model

--- models/index.js
+++ models/index.js
@@ -2,9 +2,14 @@ const Sequelize = require("sequelize");
 
 const env = process.env.NODE_ENV || "development";
 const config = require("../config/config")[env];
+const user = require("./user");
+
 const db = {};
 
 const sequelize = new Sequelize(config.database, config.username, config.password, config);
+
+user.init(sequelize);
+db.user=user;
 
 db.sequelize = sequelize;
 
