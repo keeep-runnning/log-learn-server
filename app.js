2754f9bbc9b871b6027464c6bb5a4d0e54a50a1e
jm kim <rookie2000g@gmail.com>
2022-05-02 15:42:47 +0900

    Feat: configure sequelize

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
 const express = require("express");
 
+const { sequelize } = require("./models");
+
 const app = express();
 app.set("port", process.env.PORT || 8080);
+
+sequelize.sync({ force: false })
+  .then(() => console.log("database is connected..."))
+  .catch((error) => console.error(error));
 
 app.get("/", (req, res) => {
   res.send("express");
