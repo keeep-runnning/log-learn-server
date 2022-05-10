24eda89f3bd94327da6170a47816068eb1c382ba
jm kim <rookie2000g@gmail.com>
2022-05-10 17:33:22 +0900

    Feat(routers/posts): add router to handle reading blog post

--- routers/posts.js
+++ routers/posts.js
@@ -1,6 +1,7 @@
 const express = require("express");
 
 const { Post, User } = require("../models");
+const { BusinessError } = require("../errors/BusinessError");
 
 const router = express.Router();
 
@@ -28,6 +29,38 @@ router.post("/", async (req, res, next) => {
         author: newPost.User.username,
         createdAt: newPost.createdAt
       });
+  } catch(error) {
+    console.error(error);
+    next(error);
+  }
+});
+
+router.get("/:postId", async (req, res, next) => {
+  const { postId } = req.params;
+  try {
+    const post = await Post.findOne({
+      where: { id : postId },
+      include: {
+        model: User,
+        attributes: ["username"]
+      }
+    });
+    if(!post) {
+      const postNotFoundError = new BusinessError({
+        errorCode: "post-001",
+        message: "블로그 포스트가 없습니다.",
+        errors: [],
+        statusCode: 404
+      });
+      return next(postNotFoundError);
+    }
+    return res.json({
+      id: post.id,
+      title: post.title,
+      content: post.content,
+      author: post.User.username,
+      createdAt: post.createdAt
+    });
   } catch(error) {
     console.error(error);
     next(error);
