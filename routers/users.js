2568a24c69353322dd32c29a3d8e7a24ffb4d8db
jm kim <rookie2000g@gmail.com>
2022-05-19 16:09:16 +0900

    Feat(routers/users): check if given username is already in use when signing up

--- routers/users.js
+++ routers/users.js
@@ -3,6 +3,7 @@ const bcrypt = require("bcrypt");
 
 const { User } = require("../models");
 const { validateUserCreationRequestBody } = require("./middlewares/validation");
+const { BusinessError } = require("../errors/BusinessError");
 
 const router = express.Router();
 
@@ -12,6 +13,15 @@ router.post(
   async (req, res, next) => {
     const { username, email, password: rawPassword } = req.body;
     try {
+      const userFoundByUsername = await User.findOne({ where: { username } });
+      if(userFoundByUsername) {
+        const duplicatedUsernameError = new BusinessError({
+          errorCode: "user-001",
+          message: "이미 사용중인 유저이름입니다.",
+          statusCode: 409
+        });
+        return next(duplicatedUsernameError);
+      }
       const hashedPassword = await bcrypt.hash(rawPassword, 10);
       await User.create({ username, email, password: hashedPassword });
       res.status(200).send();
